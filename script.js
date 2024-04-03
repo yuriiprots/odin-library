c400a4b2d24cfcc7547c1b2c683e6cde2f39706a
yuriiprots <uraprots7849@gmail.com>
2024-04-03 10:42:47 +0300

    create the `addBookToLibrary` function which takes the user's input and stores the new book objects into an array

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
+const form = document.getElementById("form");
+const submitBtn = document.getElementById("submit__btn");
+
+const myLibrary = [];
+
 function Book(title, author, pages, read) {
   this.title = title;
   this.author = author;
@@ -13,4 +18,26 @@ const book1 = new Book("The Hobbit", "Tolkien", 295, "not read yet");
 const book2 = new Book("The Fellowship", "Tolkien", 1255, "not read yet");
 const book3 = new Book("The Two Towers", "Tolkien", 300, "not read yet");
 
-console.log(book1.info());
+const addBookToLibrary = () => {
+  const title = document.getElementById("title").value;
+  const author = document.getElementById("author").value;
+  const pages = document.getElementById("pages").value;
+  const readCheckbox = document.getElementById("read");
+  const read = readCheckbox.checked ? "read" : "not read yet";
+
+  if (title && author && pages) {
+    const book = new Book(title, author, pages, read);
+    myLibrary.push(book);
+    form.reset();
+  } else {
+    alert("Please fill in all the fields");
+  }
+  console.log(myLibrary);
+};
+
+submitBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+  addBookToLibrary();
+});
+
+console.log(myLibrary);
